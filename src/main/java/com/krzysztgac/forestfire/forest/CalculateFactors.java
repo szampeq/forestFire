66afe653caf446405e4dca7ca4dd9f6053d96d26
KrzysztofGąciarz <59824733+szampeq@users.noreply.github.com>
2020-11-15 19:15:27 +0100

    factors definitions continued...

--- src/main/java/com/krzysztgac/forestfire/forest/CalculateFactors.java
+++ src/main/java/com/krzysztgac/forestfire/forest/CalculateFactors.java
@@ -6,6 +6,8 @@ import com.krzysztgac.forestfire.states.*;
 
 public abstract class CalculateFactors {
 
+    private static final int NUMBER_OF_STATES = 7;
+
     private static final double VERY_LOW_TRANSITION = 0.15;
     private static final double LOW_TRANSITION = 0.30;
     private static final double MEDIUM_TRANSITION = 0.50;
@@ -22,11 +24,113 @@ public abstract class CalculateFactors {
             ForestState forestState, DensityState densityState, SeasonState seasonState, WindState windState,
             WindForceState windForceState, RainfallState rainfallState, HumidityState humidityState) {
 
-        return 0.0;
+        double spreadingFireFactor = 0.0;
+
+        // FOREST STATE
+        switch (forestState) {
+            case Coniferous:
+                spreadingFireFactor += LOW_TRANSITION;
+                break;
+            case Deciduous:
+                spreadingFireFactor += HIGH_TRANSISTION;
+                break;
+            case Mixed:
+                spreadingFireFactor += MEDIUM_TRANSITION;
+                break;
+            default:
+                spreadingFireFactor += 0.0;
+                break;
+        }
+        // DENSINITY STATE
+        switch (densityState) {
+            case Poorly:
+                spreadingFireFactor = VERY_LOW_TRANSITION;
+                break;
+            case Medium:
+                spreadingFireFactor = MEDIUM_TRANSITION;
+                break;
+            case Strongly:
+                spreadingFireFactor = VERY_HIGH_TRANSITION;
+                break;
+            default:
+                spreadingFireFactor = 0.0;
+                break;
+        }
+    // SEASON STATE
+        switch (seasonState) {
+            case Spring:
+                spreadingFireFactor = VERY_LOW_TRANSITION;
+                break;
+            case Summer:
+                spreadingFireFactor = MEDIUM_TRANSITION;
+                break;
+            case Autumn:
+                spreadingFireFactor = VERY_HIGH_TRANSITION;
+                break;
+            case Winter:
+                spreadingFireFactor = VERY_HIGH_TRANSITION;
+                break;
+            default:
+                spreadingFireFactor = 0.0;
+                break;
+        }
+// WIND FORCE STATE
+        switch (windForceState) {
+            case Windless:
+                spreadingFireFactor = VERY_LOW_TRANSITION;
+                break;
+            case GentleWind:
+                spreadingFireFactor = MEDIUM_TRANSITION;
+                break;
+            case MediumWind:
+                spreadingFireFactor = VERY_HIGH_TRANSITION;
+                break;
+            case StrongWind:
+                spreadingFireFactor = VERY_HIGH_TRANSITION;
+                break;
+            default:
+                spreadingFireFactor = 0.0;
+                break;
+        }
+// RAINFALL STATE
+        switch (rainfallState) {
+            case Rainless:
+                spreadingFireFactor = VERY_LOW_TRANSITION;
+                break;
+            case Drizzle:
+                spreadingFireFactor = MEDIUM_TRANSITION;
+                break;
+            case MediumRain:
+                spreadingFireFactor = VERY_HIGH_TRANSITION;
+                break;
+            case HeavyRain:
+                spreadingFireFactor = VERY_HIGH_TRANSITION;
+                break;
+            default:
+                spreadingFireFactor = 0.0;
+                break;
+        }
+    // HUMIDITY STATE
+        switch (humidityState) {
+            case LowHumidity:
+                spreadingFireFactor = VERY_LOW_TRANSITION;
+                break;
+            case MediumHumidity:
+                spreadingFireFactor = MEDIUM_TRANSITION;
+                break;
+            case HighHumidity:
+                spreadingFireFactor = VERY_HIGH_TRANSITION;
+                break;
+            default:
+                spreadingFireFactor = 0.0;
+                break;
+        }
+
+        return spreadingFireFactor/(double)NUMBER_OF_STATES;
     }
 
     static double burningTimeFactor(
-            ForestState forestState, DensityState densityState, SeasonState seasonState, WindState windState,
+            ForestState forestState, DensityState densityState, SeasonState seasonState,
             WindForceState windForceState, RainfallState rainfallState, HumidityState humidityState) {
 
         return 0.0;
